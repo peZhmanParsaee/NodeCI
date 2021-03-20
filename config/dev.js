6a818f9caa8f82fa5604ed20896e55e2bf7f0a3c
pezhmanparsaee <p.parsaee@gmail.com>
2021-03-20 18:33:18 +0330

    Travis config

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientID:
+    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+  // mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  // mongoURI: 'mongodb://dbUser:9ggICbdYK4U26rER@cluster0-shard-00-00.0nz8o.mongodb.net:27017,cluster0-shard-00-01.0nz8o.mongodb.net:27017,cluster0-shard-00-02.0nz8o.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-6tw4d5-shard-0&authSource=admin&retryWrites=true&w=majority',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_everyone',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
+};
