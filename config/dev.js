761cc01f103d4ace96f5347020eccbb07c198539
pezhmanparsaee <p.parsaee@gmail.com>
2021-03-23 19:33:21 +0430

    add new keys to dev.js (about upload storage and redis)

--- config/dev.js
+++ config/dev.js
@@ -6,5 +6,7 @@ module.exports = {
   // mongoURI: 'mongodb://dbUser:9ggICbdYK4U26rER@cluster0-shard-00-00.0nz8o.mongodb.net:27017,cluster0-shard-00-01.0nz8o.mongodb.net:27017,cluster0-shard-00-02.0nz8o.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-6tw4d5-shard-0&authSource=admin&retryWrites=true&w=majority',
   mongoURI: 'mongodb://127.0.0.1:27017/blog_everyone',
   cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: '',
+  secretAccessKey: ''
 };
